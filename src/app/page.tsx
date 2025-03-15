58662e2b868484e70fcff60cd049eb5e631a76e9
BertovDev <bertobautista18@gmail.com>
2025-03-15 16:13:50 -0300

    Add about-me images and section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,55 @@
 import Scene from "@/components/Scene";
 import Aside from "@/components/UI/Aside";
+import Section from "@/components/UI/Section";
 
 export default function Home() {
   return (
-    <main className="relative h-screen w-full">
+    <main className="relative h-full w-full">
       <Scene />
       <Aside />
+      <Section>
+        <div className="flex items-center h-full ">
+          <div id="leftSide" className="| flex flex-col items-center flex-auto">
+            <img
+              className="relative bottom-20"
+              src="/images/SmilingFace.svg"
+              alt=""
+            />
+            <img className="relative left-25" src="/images/Heart.svg" alt="" />
+            <img
+              className="relative right-5 top-10"
+              src="/images/pokerface.svg"
+              alt=""
+            />
+          </div>
+          <div className="flex-none w-2/4 mb-20">
+            <p className="text-black font-inter font-semibold text-center text-5xl">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
+              alias eum debitis. Fugit voluptate veritatis est illo ratione
+              eaque voluptas culpa qui autem voluptates, fugiat dolores harum
+              perferendis voluptatibus laudantium. perferendis voluptatibus
+              laudantium.um. perferendis voluptatibus laudantium.
+            </p>
+          </div>
+          <div id="rightSide" className="flex flex-col items-center flex-auto">
+            <img
+              className="relative bottom-25 right-20"
+              src="/images/DumbFace.svg"
+              alt=""
+            />
+            <img
+              className="relative left-25"
+              src="/images/pokerface2.svg"
+              alt=""
+            />
+            <img
+              className="relative right-40 top-20"
+              src="/images/Flower.svg"
+              alt=""
+            />
+          </div>
+        </div>
+      </Section>
     </main>
   );
 }
