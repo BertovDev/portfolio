60dfa1cd94e03066e1925848b4fd2d1f32ec40be
BertovDev <bertobautista18@gmail.com>
2025-03-14 23:16:49 -0300

    Remove old models and add types

---
+++ src/types/model.d.ts
@@ -0,0 +1,40 @@
+type GLTFResult = GLTF & {
+  nodes: {
+    pPlane3_lambert4_0: THREE.Mesh;
+    Object_10: THREE.Mesh;
+    Object_11: THREE.Mesh;
+    Object_12: THREE.Mesh;
+    Object_5: THREE.Mesh;
+    Object_6: THREE.Mesh;
+    Object_7: THREE.Mesh;
+    Object_8: THREE.Mesh;
+    Object_9: THREE.Mesh;
+    Object_159: THREE.Mesh;
+    Object_160: THREE.Mesh;
+    Object_161: THREE.Mesh;
+    Object_157: THREE.Mesh;
+    ["Box_1-b_Boxes_0"]: THREE.Mesh;
+    Object_46: THREE.Mesh;
+    Object_109: THREE.Mesh;
+  };
+  materials: {
+    lambert4: THREE.MeshStandardMaterial;
+    VoxMaterial_250: THREE.MeshStandardMaterial;
+    VoxMaterial_251: THREE.MeshStandardMaterial;
+    VoxMaterial_252: THREE.MeshStandardMaterial;
+    VoxMaterial_225: THREE.MeshStandardMaterial;
+    VoxMaterial_227: THREE.MeshStandardMaterial;
+    VoxMaterial_229: THREE.MeshStandardMaterial;
+    VoxMaterial_231: THREE.MeshStandardMaterial;
+    VoxMaterial_249: THREE.MeshStandardMaterial;
+    label_front: THREE.MeshStandardMaterial;
+    label_back: THREE.MeshStandardMaterial;
+    label_side: THREE.MeshStandardMaterial;
+    vinyl: THREE.MeshStandardMaterial;
+    Boxes: THREE.MeshStandardMaterial;
+    ["cover.117"]: THREE.MeshStandardMaterial;
+    ["cover.138"]: THREE.MeshStandardMaterial;
+  };
+};
+
+export default GLTFResult;
