e0602c4e6a2ed1ebdf0b2a2069de34fe2f4e5323
BertovDev <bertobautista18@gmail.com>
2025-03-18 00:45:28 -0300

    Add left and bottom aside info and tips

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import Scene from "@/components/Scene";
 import Aside from "@/components/UI/Aside";
+import AsideInfo from "@/components/UI/AsideInfo";
 import Content from "@/components/UI/Content";
+import TipBar from "@/components/UI/TipBar";
 
 export default function Home() {
   return (
-    <main className="relative h-full w-full">
+    <main className="absolute h-full w-full">
+      <AsideInfo />
       <Scene />
+      <TipBar />
       <Aside />
       <Content />
     </main>
