2ad2d0a6f66d25ddf10351053153dfd9a8049c87
BertovDev <bertobautista18@gmail.com>
2025-03-20 14:16:49 -0300

    Add more pages

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,12 @@
+import Projects from "@/components/UI/Projects";
+import React from "react";
+
+type Props = {};
+
+export default function page({}: Props) {
+  return (
+    <>
+      <Projects />
+    </>
+  );
+}
---
+++ src/app/work/page.tsx
@@ -0,0 +1,14 @@
+import Work from "@/components/UI/Work";
+import React from "react";
+
+type Props = {};
+
+export default function page({}: Props) {
+  return (
+    <>
+      <div className="h-screen">
+        <Work />
+      </div>
+    </>
+  );
+}
