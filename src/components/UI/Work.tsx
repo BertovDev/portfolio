0e2874339bc6ee98ece59f22889af62f93384273
BertovDev <bertobautista18@gmail.com>
2025-03-27 16:52:27 -0300

    Update resume and correct text in abourt

--- src/components/UI/Work.tsx
+++ src/components/UI/Work.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Work() {
   return (
-    <div className="text-black text-5xl  h-full bg-red-500">
+    <div className="text-black text-5xl  h-full bg-white">
       <h1>Work</h1>
     </div>
   );
